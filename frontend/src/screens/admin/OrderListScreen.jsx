fdd95d57ceacd2eed0fead4a3b0895d4fa48287f
phong <phong@gmail.com>
2023-11-13 16:30:22 +0700

    phong commit

--- frontend/src/screens/admin/OrderListScreen.jsx
+++ frontend/src/screens/admin/OrderListScreen.jsx
@@ -10,7 +10,7 @@ const OrderListScreen = () => {
 
   return (
     <>
-      <h1>Orders</h1>
+      <h1>Danh sách đơn đặt hàng</h1>
       {isLoading ? (
         <Loader />
       ) : error ? (
@@ -54,7 +54,7 @@ const OrderListScreen = () => {
                 <td>
                   <LinkContainer to={`/order/${order._id}`}>
                     <Button variant='light' className='btn-sm'>
-                      Details
+                      Chi tiết
                     </Button>
                   </LinkContainer>
                 </td>
